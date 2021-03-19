a979bcf826841c6a84c2478cc8964aa2cfecf6d1
AndreaPineda-3010758 <74080247+AndreaPineda-3010758@users.noreply.github.com>
2021-03-18 22:56:18 -0400

    commit all

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import PlayerForm from "./Player/PlayerForm";
+import PlayerList from "./Player/PlayerList";
+import PlayerSingle from "./Player/PlayerSingle";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {},
+    };
+
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:4000/players";
+
+    //From the URL get the data  with the Response, I am going to save it on players
+    axios
+      .get(url)
+      .then((Response) => {
+        this.setState({
+          players: Response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  updateCurrentPlayer(item) {
+    this.setState({
+      currentPlayer: item,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <nav>
+            <div className="nav-wrapper blue darken-1">
+              <a href="/" className="brand-logo">Soccer Management</a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li>
+                  <a href="sass.html">Sass</a>
+                </li>
+                <li>
+                  <a href="badges.html">Components</a>
+                </li>
+                <li>
+                  <a href="collapsible.html">JavaScript</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="col s3">
+            <PlayerList
+              players={this.state.players}
+              updateCurrentPlayer={this.updateCurrentPlayer}
+            />
+          </div>
+          <div className="col s9">
+            <PlayerSingle player={this.state.currentPlayer}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <PlayerForm />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
